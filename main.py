2e8c30b9b3b226197a70360805379cbaa098bc61
Gordeeva <meowweasy@mail.ru>
2022-11-01 18:13:11 +0400

    --

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def add_desk():
 
 #метод сохранения файлика в проекте
 def save_uploaded_file(uploadedfile):
-  with open(os.path.join("/app/general_system/", uploadedfile.name), "wb") as f:
+  with open(os.path.join("app/general_system/", uploadedfile.name), "wb") as f:
      f.write(uploadedfile.getbuffer())
   return st.success("Файл сохранен!")
 
